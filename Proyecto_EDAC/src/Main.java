9795d44656d945f64709ef97636c9c721142a58c
Pavel Rodriguez <Parv0001@ce.pucmm.edu.do>
2024-04-11 12:14:51 -0400

    Modificacion para los nombres

--- Proyecto_EDAC/src/Main.java
+++ Proyecto_EDAC/src/Main.java
@@ -5,24 +5,24 @@ public class Main {
         GrafoNoDirigido grafo = new GrafoNoDirigido();
 
         // Agregar ubicaciones
-        Ubicacion ubicacionA = new Ubicacion("A");
-        Ubicacion ubicacionB = new Ubicacion("B");
-        Ubicacion ubicacionC = new Ubicacion("C");
+        Ubicacion ubicacionA = new Ubicacion("TORRE LA MALACA");
+        Ubicacion ubicacionB = new Ubicacion("PUCMM");
+        Ubicacion ubicacionC = new Ubicacion("LA VEGA");
 
         grafo.agregarUbicacion(ubicacionA);
         grafo.agregarUbicacion(ubicacionB);
         grafo.agregarUbicacion(ubicacionC);
 
         // Agregar aristas con pesos
-        grafo.agregarArista(ubicacionA, ubicacionB, 5);
-        grafo.agregarArista(ubicacionB, ubicacionC, 7);
+        grafo.agregarArista(ubicacionA, ubicacionB, 87);
+        grafo.agregarArista(ubicacionB, ubicacionC, 300);
 
         // Imprimir lista de adyacencia
         System.out.println("Lista de adyacencia:");
         grafo.imprimirLista();
 
-        // Calcular ruta más corta desde la ubicación A
-        System.out.println("\nDistancias desde la ubicación A:");
+        // Calcular ruta más corta desde la ubicación x
+        System.out.println("\nDistancias desde la ubicación: " + ubicacionA.getNombre());
         Map<Ubicacion, Integer> distancias = grafo.rutaMasCorta(ubicacionA);
         for (Map.Entry<Ubicacion, Integer> entry : distancias.entrySet()) {
             System.out.println("A " + entry.getKey().getNombre() + ": " + entry.getValue());
